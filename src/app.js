73b975e5d9e7436373ff2ac0f36f8f4c1359ff25
Jerome Lachaud <jerome.lachaud@gmail.com>
2016-03-31 18:11:07 +0100

    Keep it minimal

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import {Component} from './components/component';
-import {FriendsList} from './components/friends-list';
-
 
 export class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>App</h1>
+        <h1>Hello World</h1>
         <Component />
-        <FriendsList />
       </div>
     );
   }
